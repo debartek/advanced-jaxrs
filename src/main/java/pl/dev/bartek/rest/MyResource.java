f11a9b8240e16035d51e172c9ad4c6bdfd59bd01
debartek <bartek154@gmail.com>
2018-01-11 00:36:41 +0100

    Singelton annotation use

--- src/main/java/pl/dev/bartek/rest/MyResource.java
+++ src/main/java/pl/dev/bartek/rest/MyResource.java
@@ -1,16 +1,21 @@
 package pl.dev.bartek.rest;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("test")
+@Singleton
 public class MyResource {
+	
+	private int count;
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String testMethod() {
-		return "Hello World!";
+		this.count++;
+		return "Works! Called: "+this.count+" times";
 	}
 }
