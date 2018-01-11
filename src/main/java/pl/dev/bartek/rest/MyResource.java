1c6aa49e4042d0032b64058754886b8257e4a0e9
debartek <bartek154@gmail.com>
2018-01-11 23:55:03 +0100

    Path param and query param annotations

--- src/main/java/pl/dev/bartek/rest/MyResource.java
+++ src/main/java/pl/dev/bartek/rest/MyResource.java
@@ -1,16 +1,22 @@
 package pl.dev.bartek.rest;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("test")
+@Path("{pathParam}/test")
+
 public class MyResource {
 	
+	@PathParam("pathParam") private String pathParamExample;
+	@QueryParam("query")private String queryParamExample;
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String testMethod() {
-		return "Hello World!";
+		return "That Works!. Path param used "+pathParamExample+" and queryParam Example "+queryParamExample;
 	}
 }
